9378bfecd421f0c5b7eab3b340e2f34930312d3f
Otmar Humbel <ohumbel@gmail.com>
2019-12-18 15:25:22 +0100

    test both plain jar and jar with version

--- tests/java/org/python/core/PySystemState_standaloneMatcherTest.java
+++ tests/java/org/python/core/PySystemState_standaloneMatcherTest.java
@@ -7,10 +7,22 @@ import junit.framework.TestCase;
 public class PySystemState_standaloneMatcherTest extends TestCase {
 
     public void testWildflyUrl() {
-        String urlpath = "/C:/workspace/eclipse_bp_BASE_GRADLE/srv/wildfly-18.0.0.Final/standalone/deployments/BisonProcess.ear/lib/jython-2.5.3.3.jar/Lib/os.py";
-        String oldUrlpath = "/C:/workspace/eclipse_bp_BASE_GRADLE/srv/wildfly-18.0.0.Final/standalone/deployments/BisonProcess.ear/lib/jython-2.5.3.3.jar!/Lib/os.py";
+        String urlpath;
 
+        // new url with version
+        urlpath = "anyRoot/wildfly-18.0.0.Final/standalone/deployments/BisonProcess.ear/lib/jython-2.5.3.3.jar/Lib/os.py";
         assertTrue(STANDALONE_PATTERN.matcher(urlpath).find());
-        assertTrue(STANDALONE_PATTERN.matcher(oldUrlpath).find());
+
+        // old url with version
+        urlpath = "anyRoot/wildfly-18.0.0.Final/standalone/deployments/BisonProcess.ear/lib/jython-2.5.3.3.jar!/Lib/os.py";
+        assertTrue(STANDALONE_PATTERN.matcher(urlpath).find());
+
+        // new url without verson
+        urlpath = "anyRoot/wildfly-18.0.0.Final/standalone/deployments/BisonProcess.ear/lib/jython.jar/Lib/os.py";
+        assertTrue(STANDALONE_PATTERN.matcher(urlpath).find());
+
+        // old url without version
+        urlpath = "anyRoot/wildfly-18.0.0.Final/standalone/deployments/BisonProcess.ear/lib/jython.jar!/Lib/os.py";
+        assertTrue(STANDALONE_PATTERN.matcher(urlpath).find());
     }
 }
